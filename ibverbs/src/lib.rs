6eb012754eaae33fabf319057beb06a63d0356a2
Arye Yurkovsky <aryeyur@google.com>
2026-05-19 20:54:29 +0300

    Add qp_num to QueuePair

--- ibverbs/src/lib.rs
+++ ibverbs/src/lib.rs
@@ -2023,6 +2023,11 @@ impl QueuePair {
         self.qp
     }
 
+    /// Returns the local QP number of this QueuePair.
+    pub fn qp_num(&self) -> u32 {
+        unsafe { *self.qp }.qp_num
+    }
+
     /// Posts a linked list of Work Requests (WRs) to the Send Queue of this Queue Pair.
     ///
     /// Generates a HW-specific Send Request for the memory at `mr[range]`, and adds it to the tail
